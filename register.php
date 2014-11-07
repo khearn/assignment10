d6a4174d2552194bbf148e6821d9de08ec59ba3d
malloryjoy <malloryjoy13@gmail.com>
2014-11-07 18:47:41 -0500

    Um what

--- register.php
+++ register.php
@@ -1,60 +1,78 @@
 <?php
-include ('top.php');
-include ('header.php');
-include ('nav.php');
-
-?>
+include "top.php";
 
 
 
-<?php
-// SECTION: 1b Security
-//
-// define security variable to be used in SECTION 2a.
-
-//Save account creation info to database
-//security
-$yourURL = $domain . $phpSelf;
-
-//variables
-$email = "";
-$password = "";
-$fname = "";
-$lname = "";
-$myFavorite = 1;
-$profession = "";
-
-//form error flags
-$emailERROR = false;
-$passwordERROR = false;
-$fnameERROR = false;
-$lnameERROR = false;
-$professionERROR = false;
-
-
-// create array to hold error messages filled (if any) in 2d displayed in 3c.
-$errorMsg = array();
-$dataRecord = array();
-
-if (isset($_POST["btnSubmit"])) {
 //%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
 //
 // SECTION: 1 Initialize variables
 //
 // SECTION: 1a.
 // variables for the classroom purposes to help find errors.
-    $debug = false;
-    if (isset($_GET["debug"])) { // ONLY do this in a classroom environment
-        $debug = true;
-    }
-    if ($debug)
-        print "<p>DEBUG MODE IS ON</p>";
+
+$debug = false;
+
+if (isset($_GET["debug"])) { // ONLY do this in a classroom environment
+    $debug = true;
+}
+
+if ($debug)
+    print "<p>DEBUG MODE IS ON</p>";
+
+require_once('../bin/myDatabase.php');
+
+    $dbUserName = get_current_user() . '_writer';
+    $whichPass = "w"; //flag for which one to use.
+    $dbName = strtoupper(get_current_user()) . '_CRUD';
+	$thisDatabase = new myDatabase($dbUserName, $whichPass, $dbName);
+//%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+//
+// SECTION: 1b Security
+//
+// define security variable to be used in SECTION 2a.
+$yourURL = $domain . $phpSelf;
+
+
+//%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+//
+// SECTION: 1c form variables
+//
+// Initialize variables one for each form element
+// in the order they appear on the form
+$user = "";
+$email = "";
+$confirm = "";
+$headers = "";
+
+
+//%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+//
+// SECTION: 1d form error flags
+//
+// Initialize Error Flags one for each form element we validate
+// in the order they appear in section 1c.
+$userERROR = false;
+$emailERROR = false;
+
+//%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+//
+// SECTION: 1e misc variables
+//
+// create array to hold error messages filled (if any) in 2d displayed in 3c.
+$errorMsg = array();
+
+// array used to hold form values that will be written to a CSV file
+$dataRecord = array();
+
+$mailed=false; // have we mailed the information to the user?
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 //
 // SECTION: 2 Process for when the form is submitted
+//
+//
 if (isset($_POST["btnSubmit"])) {
 
-	$fname = htmlentities($_POST["txtfname"], ENT_QUOTES, "UTF-8");
-	$lname = htmlentities($_POST["txtlname"], ENT_QUOTES, "UTF-8");
+	$user = htmlentities($_POST["txtUser"], ENT_QUOTES, "UTF-8");
 	$email = htmlentities($_POST["txtEmail"], ENT_QUOTES, "UTF-8");
 	//creates jumbled up thing to equal email
 	$confirm = sha1($email);
@@ -63,7 +81,7 @@ if (isset($_POST["btnSubmit"])) {
 
 
 
-$query = "INSERT INTO tblUser(fldFirstName, fldLastName, fldEmail, fldHash, fldApprove) VALUES ('". $fname ."', '". $lname ."', '". $email ."' , '". $confirm ."', '". $approved ."')";
+$query = "INSERT INTO tblUser(fldUser, fldEmail, fldHash, fldApprove) VALUES ('". $user ."', '". $email ."' , '". $confirm ."', '". $approved ."')";
             
             
             
@@ -86,37 +104,51 @@ if ($debug) {
     /* since it is associative array display the field names */
   
 
-// SECTION: 2a Security
-// 
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    //
+    // SECTION: 2a Security
+    // 
     if (!securityCheck(true)) {
         $msg = "<p>Sorry you cannot access this page. ";
         $msg.= "Security breach detected and reported</p>";
         die($msg);
     }
 
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-//
-// SECTION: 2b Sanitize (clean) data 
-// remove any potential JavaScript or html code from users input on the
-// form. Note it is best to follow the same order as declared in section 1c.
+
+    
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    //
+    // SECTION: 2b Sanitize (clean) data 
+    // remove any potential JavaScript or html code from users input on the
+    // form. Note it is best to follow the same order as declared in section 1c.
+
+    $user = htmlentities($_POST["txtUser"], ENT_QUOTES, "UTF-8");
+    $dataRecord[] = $user;
 
 
     $email = filter_var($_POST["txtEmail"], FILTER_SANITIZE_EMAIL);
     $dataRecord[] = $email;
 
-    $password = htmlentities($_POST["pwdPassword"], ENT_QUOTES, "UTF-8");
-    $dataRecord[] = $password;
 
-    $fname = htmlentities($_POST["txtfname"], ENT_QUOTES, "UTF-8");
-    $dataRecord[] = $fname;
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    //
+    // SECTION: 2c Validation
+    //
+    // Validation section. Check each value for possible errors, empty or
+    // not what we expect. You will need an IF block for each element you will
+    // check (see above section 1c and 1d). The if blocks should also be in the
+    // order that the elements appear on your form so that the error messages
+    // will be in the order they appear. errorMsg will be displayed on the form
+    // see section 3b. The error flag ($emailERROR) will be used in section 3c.
 
-    $lname = htmlentities($_POST["txtlname"], ENT_QUOTES, "UTF-8");
-    $dataRecord[] = $lname;
+    if ($user == "") {
+        $errorMsg[] = "Please choose a username";
+        $userERROR = true;
+    } elseif (!verifyAlphaNum($user)) {
+        $errorMsg[] = "Your username appears to have extra character.";
+        $userERROR = true;
+    }
 
-
-
-
-    //validate
     if ($email == "") {
         $errorMsg[] = "Please enter your email address";
         $emailERROR = true;
@@ -125,40 +157,24 @@ if ($debug) {
         $emailERROR = true;
     }
 
-    if ($password == "") {
-        $errorMsg[] = "Please enter a password";
-        $passwordERROR = true;
-    } elseif (!verifyAlphaNum($password)) {
-        $errorMsg[] = "Your password appears to have extra character.";
-        $passwordERROR = true;
-    }
 
-    if ($fname == "") {
-        $errorMsg[] = "Please enter your first name";
-        $fnameERROR = true;
-    } elseif (!verifyAlphaNum($fname)) {
-        $errorMsg[] = "Your first name appears to have extra character.";
-        $fnameERROR = true;
-    }
-
-    if ($lname == "") {
-        $errorMsg[] = "Please enter your last name";
-        $lnameERROR = true;
-    } elseif (!verifyAlphaNum($lname)) {
-        $errorMsg[] = "Your last name appears to have extra character.";
-        $lnameERROR = true;
-    }
-
-
-
-    //If pass validation
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    //
+    // SECTION: 2d Process Form - Passed Validation
+    //
+    // Process for when the form passes validation (the errorMsg array is empty)
+    //
     if (!$errorMsg) {
         if ($debug)
             print "<p>Form is valid</p>";
-    		} 
-    // end form is valid
-// execute query using a  prepared statement
-}
+
+        //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        //
+        // SECTION: 2e Save Data
+        //
+        // This block saves the data to a CSV file.
+
+       
 
         //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
         //
@@ -188,7 +204,6 @@ if ($debug) {
         // subject of mail should make sense to your form
         $todaysDate = strftime("%x");
         $subject = "Welcome to the Real World: " . $todaysDate;
-        $headers = " ";
 
         mail($to, $subject, $message, $headers);
         
@@ -196,163 +211,122 @@ if ($debug) {
     
 } // ends if form was submitted.
 
-
-//Display form
+//#############################################################################
+//
+// SECTION 3 Display Form
+//
 ?>
 
-
-
-
-<article>
+<article id="main">
 
     <?php
+    //####################################
+    //
+    // SECTION 3a.
+    //
+    // 
+    // 
+    // 
+    // If its the first time coming to the form or there are errors we are going
+    // to display the form.
     if (isset($_POST["btnSubmit"]) AND empty($errorMsg)) { // closing of if marked with: end body submit
-        print "<h1>A confirmation message has ";
-        if (!$mailed) {
-            print "not ";
-        }
-        print "been sent to your email</h1>";
-        print "<p>A copy of this message has ";
-        if (!$mailed) {
-            print "not ";
-        }
-        print "been sent</p>";
-        print "<p>To: " . $email . "</p>";
-        print "<p>Mail Message:</p>";
-        print $message;
+        print "<h1> Congratulations! You should receive a confirmation email shortly. </h1>";
+
+     
+
+        
     } else {
+
+
+        //####################################
+        //
+        // SECTION 3b Error Messages
+        //
+        // display any error messages before we print out the form
+
+        if ($errorMsg) {
+            print '<div id="errors">';
+            print "<ol>\n";
+            foreach ($errorMsg as $err) {
+                print "<li>" . $err . "</li>\n";
+            }
+            print "</ol>\n";
+            print '</div>';
+        }
+
+
+        //####################################
+        //
+        // SECTION 3c html Form
+        //
+        /* Display the HTML form. note that the action is to this same page. $phpSelf
+          is defined in top.php
+          NOTE the line:
+
+          value="<?php print $email; ?>
+
+          this makes the form sticky by displaying either the initial default value (line 35)
+          or the value they typed in (line 84)
+
+          NOTE this line:
+
+          <?php if($emailERROR) print 'class="mistake"'; ?>
+
+          this prints out a css class so that we can highlight the background etc. to
+          make it stand out that a mistake happened here.
+
+         */
         ?>
 
-        <form action="/cs148/assignment10/register.php"
+        <form action="<?php print $phpSelf; ?>"
               method="post"
               id="frmRegister">
-            <fieldset class="register">
-                <legend>Sign Up!</legend>
 
-                <label for="txtfname">First Name
-                    <input type="text" id="txtfname" name="txtfname"
-                           value="<?php print $fname ?>"
-                           tabindex="100" maxlength="45" placeholder="Please enter your First Name"
-                           <?php if ($fnameERROR) print 'class="mistake"'; ?>
-                           onfocus="this.select()"
-                           >
-                </label>
+            <fieldset class="wrapper">
+                <legend>Register Today!</legend>
+               
 
-                <label for="txtlname">Last Name
-                    <input type="text" id="txtlname" name="txtlname"
-                           value="<?php print $lname ?>"
-                           tabindex="200" maxlength="45" placeholder="Please enter your Last Name"
-                           <?php if ($lnameERROR) print 'class="mistake"'; ?>
-                           onfocus="this.select()"
-                           >
-                </label>
+                <fieldset class="wrapperTwo">
+                    <legend>Please complete the following form</legend>
 
-                <label for="txtEmail">Email
-                    <input type="text" id="txtEmail" name="txtEmail"
-                           value="<?php print $email ?>"
-                           tabindex="300" maxlength="45" placeholder="Please enter a valid Email Address"
-                           <?php if ($emailERROR) print 'class="mistake"'; ?>
-                           onfocus="this.select()"
-                           >
-                </label>
-
-                <label for="txtPassword">Password
-                    <input type="password" id="pwdPassword" name="pwdPassword"
-                           value="<?php print $password ?>"
-                           tabindex="400" maxlength="45" placeholder="Please enter a valid Email Address"
-                           <?php if ($passwordERROR) print 'class="mistake"'; ?>
-                           onfocus="this.select()"
-                           >
-                </label>
-
-                <fieldset class="radio">
-                    <legend>Gender:</legend>
-                    <label for="radFem">
-                        <input type="radio" 
-                               id="radFem" 
-                               name="radGender" 
-                               value="2">Female
-                    </label>
-                    <label for="radMale">
-                        <input type="radio" 
-                               id="radMale" 
-                               name="radGender" 
-                               value="3">Male
-                    </label>
-                </fieldset>
-
-                <fieldset class="checkbox">
-                    <legend>Please check all that apply to you:</legend>
-                    <label for="chkParent">
-                        <input type="checkbox" 
-                               id="chkParent" 
-                               name="chkParent" 
-                               value="2">Parent
-                    </label>
-                    <label for="chkStudent">
-                        <input type="checkbox" 
-                               id="chkStudent" 
-                               name="chkStudent" 
-                               value="3">Student
-                    </label>
-                    <label for="chkEmployed">
-                        <input type="checkbox" 
-                               id="chkEmployed" 
-                               name="chkEmployed" 
-                               value="4">Employed
-                    </label>
-                    <label for="chkPet">
-                        <input type="checkbox" 
-                               id="chkPet" 
-                               name="chkPet" 
-                               value="5">Pet Owner
-                    </label>
-                    <label for="chkMarried">
-                        <input type="checkbox" 
-                               id="chkMarried" 
-                               name="chkMarried" 
-                               value="6">Married
-                    </label>
-                    <label for="chkTravel">
-                        <input type="checkbox" 
-                               id="chkTravel" 
-                               name="chkTravel" 
-                               value="7">Frequent Traveler
-                    </label>
+                    <fieldset class="contact">
+                        <legend>Contact Information</legend>
+                        <label for="txtUser" class="required">User Name
+                            <input type="text" id="txtUser" name="txtUser"
+                                   value="<?php print $user; ?>"
+                                   tabindex="100" maxlength="45" placeholder="Please select a username"
+                                   <?php if ($userERROR) print 'class="mistake"'; ?>
+                                   onfocus="this.select()"
+                                   autofocus>
+                        </label>
+                        
+                        <label for="txtEmail" class="required">Email
+                            <input type="text" id="txtEmail" name="txtEmail"
+                                   value="<?php print $email; ?>"
+                                   tabindex="120" maxlength="45" placeholder="Enter a valid email address"
+                                   <?php if ($emailERROR) print 'class="mistake"'; ?>
+                                   onfocus="this.select()" 
+                                   >
+                        </label>
+                    </fieldset> <!-- ends contact -->
                     
-                    
-
-                    <!--
-                    <label for="chkProfession">
-                        <input type="checkbox" 
-                               id="chkProfession" 
-                               name="chkProfession" 
-                               value="3">Profession: 
-                                    <input type="text" name="profession"
-                                           value="<?php //print $profession ?>"
-                                           tabindex="500" maxlength="45" placeholder="Please enter your job title"
-                    <?php //if ($professionERROR) print 'class="mistake"'; ?>
-                                           onfocus="this.select()"
-                                    >
-                                           
-                    </label>
-                    -->
-                </fieldset>
-                <input type="submit" id="btnSubmit" name="btnSubmit" value="Sign Up" tabindex="900" class="button">
-
-            </fieldset>
-
+                </fieldset> <!-- ends wrapper Two -->
+                
+                <fieldset class="buttons">
+                    <legend></legend>
+                    <input type="submit" id="btnSubmit" name="btnSubmit" value="Register" tabindex="900" class="button">
+                </fieldset> <!-- ends buttons -->
+                
+            </fieldset> <!-- Ends Wrapper -->
         </form>
 
-    </article>
-
     <?php
-}
-?>
+    } // end body submit
+    ?>
 
-<?php
-include ('footer.php');
-?>
+</article>
+
+<?php include "footer.php"; ?>
 
 </body>
 </html>
--- top.php
+++ top.php
@@ -1,46 +1,73 @@
+<?php ini_set('display_errors',1); error_reporting(E_ALL); ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <title>Random Task</title>
         <meta charset="utf-8">
-        <meta name="author" content="Mallory Joy & Kylie Hearn">
-        <meta name="description" content="Random Task is a website that reminds you of things you have to do. The idea is that you will submit a task and on the day you have to do it, we will send you either an email or a text message reminding you to do said task.">
+        <meta name="author" content="Mallory">
+        <meta name="description" content="Homework">
+
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!--[if lt IE 9]>
         <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
         <![endif]-->
 
-        <link rel="stylesheet" href="randomTask.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="style.css" type="text/css" media="screen">
 
-        <!--        <link rel="shortcut icon" href="eighth-note.ico" type="image/x-icon">
-                <link rel="icon" href="eighth-note.ico" type="image/x-icon">-->
-		<?php
-		$debug = false;
 
-		if (isset($_GET["debug"])) { // ONLY do this in a classroom environment
-    	$debug = true;
-    	}
-		?>
-		
-	<?php
-		if ($debug)
-	    print "<p>DEBUG MODE IS ON</p>";
-        
-        require_once('../bin/myDatabase.php');
+        <?php
+        $debug = false;
 
-        $dbUserName = 'mljoy_writer';
-        $whichPass = "w"; //flag for which one to use.
-        $dbName = 'MLJOY_RANDOM_TASK';
-        $thisDatabase = new myDatabase($dbUserName, $whichPass, $dbName);
-    ?>
+// %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+//
+// PATH SETUP
+//
+//  $domain = "https://www.uvm.edu" or http://www.uvm.edu;
+
+        $domain = "http://";
+        if (isset($_SERVER['HTTPS'])) {
+            if ($_SERVER['HTTPS']) {
+                $domain = "https://";
+            }
+        }
+
+        $server = htmlentities($_SERVER['SERVER_NAME'], ENT_QUOTES, "UTF-8");
+
+        $domain .= $server;
+
+        $phpSelf = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8");
+
+        $path_parts = pathinfo($phpSelf);
+
+        if ($debug) {
+            print "<p>Domain" . $domain;
+            print "<p>php Self" . $phpSelf;
+            print "<p>Path Parts<pre>";
+            print_r($path_parts);
+            print "</pre>";
+        }
+
+// %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+//
+// inlcude all libraries
+//
+
+        require_once('lib/security.php');
+
+        //if ($path_parts['filename'] == "form" || $path_parts['filename'] == "crud") {
+            require "lib/validation_functions.php";
+            require "lib/mailMessage.php";
+        //}
+        ?>	
 
     </head>
+    <!-- ################ body section ######################### -->
 
     <?php
-    	print '<body id="' . $path_parts['filename'] . '">';
-    ?>
+    print '<body id="' . $path_parts['filename'] . '">';
     
-ini_set('display_errors',l);
-error_reporting(E_ALL);
+    include "header.php";
+    include "nav.php";
+    ?>
     
